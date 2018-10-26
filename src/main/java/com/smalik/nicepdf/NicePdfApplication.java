4065daa3d668c586b078ba334b01d7b0b9b0d09e
Salman Malik <smalik@thinkstation.lan>
2018-10-26 12:42:28 -0400

    initial commit

---
+++ src/main/java/com/smalik/nicepdf/NicePdfApplication.java
@@ -0,0 +1,12 @@
+package com.smalik.nicepdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicePdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicePdfApplication.class, args);
+	}
+}
